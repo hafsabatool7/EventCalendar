58c0f9f0fc8b775668a1c9ec4c0c05fc40b2088b
hafsabatool7 <52164760+hafsabatool7@users.noreply.github.com>
2019-07-04 12:23:23 +0500

    Delete event.rb

--- event.rb
+++
@@ -1,9 +0,0 @@
-# This class is the event class which stores all information related to an event
-
-class Event
-  attr_accessor :name, :description
-  def initialize(ename, description)
-    @name = ename
-    @description = description
-  end
-end
